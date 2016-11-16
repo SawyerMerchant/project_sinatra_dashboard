b0cbb4e6cafdc3f89dae93dd1222698303d9215d
Jonathan Popenuck <john.popenuck@gmail.com>
2016-11-16 15:27:39 -0800

    styled table started working on geo locate

--- app.rb
+++ app.rb
@@ -1,18 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'thin'
+require 'open-uri'
 require_relative 'lib/job_search'
 
 
 # homepage allows user to search for keyword and a location
 
 get "/" do
+
   term = params[:term] || "ruby"
-  location = params[:location] || "33603"
+  location = params[:location] || open("freegeoip.net/json/#{request.ip}")
 
   jobs = JobSearch.new(term, location)
 
   erb :index, locals:{jobs: jobs}
+
 end
 
 post "/" do
