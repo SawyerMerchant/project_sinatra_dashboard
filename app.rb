d1a5428547046c1ad1c260cca505d26a54183448
John Sawyer <sawyer@sawyermerchant.com>
2016-11-16 19:08:08 -0500

    Fixed what we broke

--- app.rb
+++ app.rb
@@ -3,14 +3,17 @@ require 'sinatra/reloader'
 require 'thin'
 require 'open-uri'
 require_relative 'lib/job_search'
+require 'json'
+require 'pry'
+require 'pry-byebug'
 
 
 # homepage allows user to search for keyword and a location
-
+# file_contents = open('local-file.txt') { |f| f.read }
 get "/" do
-
-  term = params[:term] || "ruby"
-  location = params[:location] || open("freegeoip.net/json/#{request.ip}")
+  ip = JSON.parse(`curl freegeoip.net/json/`)
+  location = ip["zip_code"] || ip["city"] || ip["region_name"]
+  term = "developer"
 
   jobs = JobSearch.new(term, location)
 
